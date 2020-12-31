622686f08632eb71d9ab453db5165dacc5f704f9
Drew Vena <drewvena1@gmail.com>
2020-12-31 17:25:46 -0600

    trying to get for loop to work for questions array

--- assets/coding.js
+++ assets/coding.js
@@ -1,28 +1,28 @@
 var questions = [
     {
-        title: "Commonly used data types DO NOT include:",
+        q: "Commonly used data types DO NOT include:",
         choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
+        a: "alerts"
     },
     {
-        title: "The condition in an if / else statement is enclosed within ____.",
+        q: "The condition in an if / else statement is enclosed within ____.",
         choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
+        a: "parentheses"
     },
     {
-        title: "Arrays in Javascript can be used to store ____.",
+        q: "Arrays in Javascript can be used to store ____.",
         choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        answer: "all of the above"
+        a: "all of the above"
     },
     {
-        title: "String values must be enclosed within ____ when being assigned to variables.",
+        q: "String values must be enclosed within ____ when being assigned to variables.",
         choices: ["commas", "curly brackets", "quotes", "parenthesis"],
-        answer: "quotes"
+        a: "quotes"
     },
     {
-        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+            q: "A very useful tool for used during development and debugging for printing content to the debugger is:",
         choices: ["Javascript", "terminal / bash", "for loops", "console log"],
-        answer: "console log"
+        a: "console log"
     },
 
 ];
@@ -39,3 +39,12 @@ var penalty = 10;
 var ulCreate = document.createElement("ul");
 
 
+function render(questionIndex) { 
+    questionsDiv.innerHTML = "";
+    ulCreate.innerHTML = "";
+    for (var i = 0; i < questions.length; i++) {
+        var userQuestion = questions[questionIndex].title;
+        questionsDiv.textContent = userQuestion;
+    }
+ 
+}
